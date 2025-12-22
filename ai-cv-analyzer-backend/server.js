23e13f2ade226d3742d55f5d3715ee045f0f72ff
Maulida14 <11231038@student.itk.ac.id>
2025-12-22 20:33:09 +0800

    Update lagi

--- ai-cv-analyzer-backend/server.js
+++ ai-cv-analyzer-backend/server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const upload = multer({ dest: "uploads/" });
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // API Keys & Config
 const Hf = new HfInference(process.env.HF_API_KEY);
