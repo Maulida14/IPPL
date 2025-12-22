2e6c45c7585e0ba164a1eadfcbfa5d22c7450b3b
Maulida14 <11231038@student.itk.ac.id>
2025-12-22 21:12:09 +0800

    Update lagi

--- ai-cv-analyzer-backend/server.js
+++ ai-cv-analyzer-backend/server.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const geminiModel = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+const geminiModel = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 
 const app = express();
