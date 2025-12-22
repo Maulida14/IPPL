9a28a6e49c2f72328aeeddc117925e9aee17e33f
Maulida14 <11231038@student.itk.ac.id>
2025-12-22 20:55:10 +0800

    Update lagi

--- ai-cv-analyzer-backend/server.js
+++ ai-cv-analyzer-backend/server.js
@@ -14,7 +14,11 @@ const geminiModel = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const upload = multer({ dest: "uploads/" });
