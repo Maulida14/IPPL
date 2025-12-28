ca0f64d5a6af7fe73aaba80ec2a536d1761210c4
Maulida14 <11231038@student.itk.ac.id>
2025-12-28 21:52:07 +0800

    update lagi

--- ai-cv-analyzer-frontend/script.js
+++ ai-cv-analyzer-frontend/script.js
@@ -374,7 +374,9 @@ if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
-        themeText.textContent = "switch to light mode"; 
+        themeText.textContent = "switch to light mode";
+    } else {
+        themeText.textContent = "switch to dark mode";
     }
 }
 
